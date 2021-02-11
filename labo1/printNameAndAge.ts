f6b2074aaa4d62bf8e9662a9bf7e23695f5b5dd0
mahmoud ahmed mohammedomer <mahmoud.ahmedmohammedomer@student.ap.be>
2021-02-11 21:29:11 +0100

    Labo 1 gedaan

---
+++ labo1/printNameAndAge.ts
@@ -0,0 +1,5 @@
+let Voornaam : string = "mahmoud";
+let Familienaam : string = "Ahmed"
+let age = 32;
+let introduction : string = `Ik ben ${Voornaam} ${Familienaam} en ik ben ${age} jaar oud.`;
+console.log(introduction);
