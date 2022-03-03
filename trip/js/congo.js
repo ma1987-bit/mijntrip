b837ff344a794906786163395f1477e9d9c100f8
Ahmed Mohamed Omer Mahmoud (s123157) <mahmoud.ahmedmohamedomer@student.ap.be>
2022-03-03 21:17:57 +0100

    trip opdracht

---
+++ trip/js/congo.js
@@ -0,0 +1,15 @@
+let myMainMap;
+
+
+myMainMap = L.map("mainMap").setView([51.22970314028477, 4.404691553960607], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibGVjdG9yd291dGVyIiwiYSI6ImNrM25qZWs1dTB4NHgza240bW0zOG1qZngifQ.1uF5JjJA8l5SpTW3NVQJJQ'
+}).addTo(myMainMap);
+
+let MAS = L.marker([51.22970314028477, 4.404691553960607]).addTo(myMainMap);
+MAS.bindPopup("Het MAS museum de best plaats te bezoeken").openPopup();
